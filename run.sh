efc5083a149837883ee395c3de9c9cf8aa2ebd9a
nagizero <nagizero@foxmail.com>
2018-02-28 16:32:58 +0800

    run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+python train.py --id st --caption_model show_tell --input_json data/cocotalk.json --input_fc_dir data/cocotalk_fc --input_att_dir data/cocotalk_att --input_label_h5 data/cocotalk_label.h5 --batch_size 64 --learning_rate 2e-4 --learning_rate_decay_start 0 --scheduled_sampling_start 0 --checkpoint_path log_st --save_checkpoint_every 2000 --val_images_use 2000 --max_epochs 25
