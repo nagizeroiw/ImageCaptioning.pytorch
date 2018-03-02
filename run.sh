cc5209f993fd359760b89e50b0d0119d40b14097
nagizero <nagizero@foxmail.com>
2018-03-02 10:00:38 +0800

    minor

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-python train.py --id st --caption_model show_tell \
+ID=topdown_test
+python train.py --id st --caption_model topdown \
     --input_json data/cocotalk.json \
     --input_fc_dir data/cocotalk_fc --input_att_dir data/cocotalk_att \
     --input_label_h5 data/cocotalk_label.h5 \
     --batch_size 64 --learning_rate 2e-4 --learning_rate_decay_start 0 \
     --scheduled_sampling_start 0 \
-    --checkpoint_path log_st --save_checkpoint_every 2000 \
+    --checkpoint_path log_$ID --save_checkpoint_every 2000 \
     --cmd_log_every 100 \
-    --val_images_use 2000 --max_epochs 50 \
-    --start_from log_st
+    --val_images_use 2000 --max_epochs 25
