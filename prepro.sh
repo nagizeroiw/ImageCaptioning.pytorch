a119a9a04b9c412afcfa4918a53aa790a68e95a6
nagizero <nagizero@foxmail.com>
2018-03-05 14:56:44 +0800

    minor

--- prepro.sh
+++ prepro.sh
@@ -1,3 +1,3 @@
-python scripts/prepro_labels.py --input_json data/msvd_dataset/dataset_msvd.json --output_json data/msvd_dataset/msvd.json --output_h5 data/msvd_dataset/msvd
+# python scripts/prepro_labels.py --input_json data/msvd_dataset/dataset_msvd.json --output_json data/msvd_dataset/msvd.json --output_h5 data/msvd_dataset/msvd
 
-# python scripts/prepro_feats.py --input_json data/msvd_dataset/dataset_msvd.json --output_dir data/msvd_dataset/msvd --images_root ~/video_caption/msvd/sampled_video_frames/
+python scripts/prepro_feats.py --input_json data/msvd_dataset/dataset_msvd.json --output_dir data/msvd_dataset/msvd --images_root ~/video_caption/msvd/sampled_video_frames/
