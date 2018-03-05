292566a6b2844681cfb8b62983638894a1b2e076
Ke Su <nagizero@ALTEREGO.localdomain>
2018-03-05 14:42:53 +0800

    minor

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 ID=topdown_test
 python train.py --id st --caption_model topdown \
-    --input_json data/cocotalk.json \
-    --input_fc_dir data/cocotalk_fc --input_att_dir data/cocotalk_att \
-    --input_label_h5 data/cocotalk_label.h5 \
+    --input_json data/msvd_dataset/dataset_msvd.json \
+    --input_fc_dir data/msvd_dataset/msvd_fc --input_att_dir data/msvd_dataset/msvd_att \
+    --input_label_h5 data/msvd_label.h5 \
     --batch_size 64 --learning_rate 2e-4 --learning_rate_decay_start 0 \
     --scheduled_sampling_start 0 \
     --checkpoint_path log_$ID --save_checkpoint_every 2000 \
