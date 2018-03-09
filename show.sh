998ea49337331c5ea9bb624ca72922e58f656b45
nagizero <nagizero@foxmail.com>
2018-03-09 11:34:42 +0800

    minor

--- show.sh
+++ show.sh
@@ -1,5 +1,5 @@
 ID=later_decay
-python train.py --id $ID --caption_model topdown \
+python show.py --id $ID --caption_model topdown --start_from ./log_$ID \
     --input_json data/msvd_dataset/msvd.json \
     --input_fc_dir data/msvd_dataset/msvd_fc --input_att_dir data/msvd_dataset/msvd_att \
     --input_label_h5 data/msvd_dataset/msvd_label.h5 \
@@ -7,4 +7,4 @@ python train.py --id $ID --caption_model topdown \
     --scheduled_sampling_start 0 \
     --checkpoint_path log_$ID --save_checkpoint_every 1500 \
     --cmd_log_every 100 \
-    --val_images_use 670 --max_epochs 400
+    --val_images_use 10 --max_epochs 400
