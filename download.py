2e7059e8063913660d6e66b4ddbbfbcb7b24bb85
Ke Su <nagizero@ALTEREGO.localdomain>
2018-03-11 14:19:31 +0800

    minor

---
+++ download.py
@@ -0,0 +1,22 @@
+import cPickle
+import sys
+
+vid2name_path = {
+    'msvd': './data/msvd_raw/vid2name.pkl',
+    'kuaishou': './data/kuaishou_raw/vid2name.pkl'
+}
+
+vid_url = {
+    ''
+}
+
+def download():
+    pass
+
+def main(dataset, vid):
+
+
+if __name__ == '__main__':
+    dataset = sys.argv[1]
+    vid = int(sys.argv[2])
+    main(dataset, vid)
