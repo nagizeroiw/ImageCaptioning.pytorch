918ced1a2da694524894e0bcf0accf73eeb150e6
Ke Su <nagizero@ALTEREGO.localdomain>
2018-03-11 14:30:39 +0800

    minor

--- download.py
+++ download.py
@@ -1,5 +1,6 @@
 import cPickle
 import sys
+import os
 
 vid2name_path = {
     'msvd': './data/msvd_raw/vid2name.pkl',
@@ -7,14 +8,13 @@ vid2name_path = {
 }
 
 vid_url = {
-    ''
+    'msvd': 'jungpu2:~/vc/data/msvd_raw/YouTubeClips/',
+    'kuaishou': 'jungpu4:~/video_caption/kuaishou_dataset/caption_video_thu/'
 }
 
-def download():
-    pass
-
 def main(dataset, vid):
-
+    vid2name = cPickle.load(open(vid2name_path[dataset]))
+    os.system('scp %s%s ./clips/%s_%d.%s' % (vid_url[dataset], vid2name[vid], dataset, vid, vid2name[vid].split('.')[-1]))
 
 if __name__ == '__main__':
     dataset = sys.argv[1]
