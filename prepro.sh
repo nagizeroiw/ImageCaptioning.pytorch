5de10aebf675f84d6d2ff071853c4c2161f4de3e
nagizeroiw <nagizero@foxmail.com>
2018-03-20 09:39:34 +0800

    minor

--- prepro.sh
+++ prepro.sh
@@ -1,3 +1,3 @@
-python scripts/prepro_labels.py --input_json data/kuaishou_dataset/dataset_kuaishou.json --output_json data/kuaishou_dataset/kuaishou.json --output_h5 data/kuaishou_dataset/kuaishou
+# python scripts/prepro_labels.py --input_json data/kuaishou_dataset/dataset_kuaishou.json --output_json data/kuaishou_dataset/kuaishou.json --output_h5 data/kuaishou_dataset/kuaishou
 
 python scripts/prepro_feats.py --input_json data/kuaishou_dataset/dataset_kuaishou.json --output_dir data/kuaishou_dataset/kuaishou --images_root ~/vc/data/kuaishou_raw/video_frames/
